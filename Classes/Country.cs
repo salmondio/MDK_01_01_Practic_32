17906ee9b234df8c8d2e33433b898c1495646339
asdasd <adadad>
2026-03-26 15:18:28 +0500

    РАЗРАБОТАН КЛАСС STATE И COUNTRY сЕРАЖНИН

---
+++ Classes/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Classes
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static IEnumerable<Country> All()
+        {
+            List<Country> all = new List<Country>();
+            DataTable countyTable = DBConnection.Connection("SELECT * FROM [dbo].[Country]");
+
+            foreach(DataRow row in countyTable.Rows)
+            {
+                all.Add(new Country() {
+                    Id = Convert.ToInt32(row[0]),
+                    Name = row[1].ToString()
+                });
+            }
+
+            return all;
+        }
+    }
+}
--- Classes/DBConnection.cs
+++ Classes/DBConnection.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Data.SqlTypes;
 
 namespace WpfApp1.Classes
 {
